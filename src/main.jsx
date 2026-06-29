f36a52f95c9ec34440211332c4d0d589f12a31a4
JLO <totoyjose11@gmail.com>
2026-06-29 13:18:45 +0800

    vercel v1.5.3

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 // PERFORMANCE: Import icon fonts so they are bundled by Vite.
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "lineicons/dist/lineicons.css"; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Import main stylesheet last to allow overriding library styles.
 import './index.css';
